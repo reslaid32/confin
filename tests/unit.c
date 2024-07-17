e78a014afebfb81952b3383274bb7d3d6d065ed9
Hayes <zangetsuebaw@gmail.com>
2024-07-17 18:23:12 +0300

    Update unit.c
    Added testing of structures in confin entry

--- tests/unit.c
+++ tests/unit.c
@@ -9,22 +9,30 @@
 #include "../confin/cfutils.h"
 #include "../confin/cffmt.h"
 
+struct Structure {
+    int x, y;
+};
+
 // Function to create sample entries
 void create_sample_entries(cfentry_t *entries, uint64_t *entry_count) {
     int int_value = 42;
     float float_value = 3.14f;
     const char *string_value = "Hello, World!";
+    struct Structure structure;
+    structure.x = 20;
+    structure.y = 40;
 
     entries[0] = cfcreatecfgentry("int_key", CONFIN_ANNOTYPE_INT, &int_value, sizeof(int_value));
     entries[1] = cfcreatecfgentry("float_key", CONFIN_ANNOTYPE_FLOAT, &float_value, sizeof(float_value));
     entries[2] = cfcreatecfgentry("string_key", CONFIN_ANNOTYPE_STRING, string_value, strlen(string_value) + 1);
+    entries[3] = cfcreatecfgentry("structure", CONFIN_ANNOTYPE_STRUCT, (void*)&structure, sizeof(structure));
 
-    *entry_count = 3;
+    *entry_count = 4;
 }
 
 // Test for writing and reading configuration file
 void test_write_read_config() {
-    cfentry_t entries[3];
+    cfentry_t entries[4];
     uint64_t entry_count;
     create_sample_entries(entries, &entry_count);
 
@@ -57,6 +65,13 @@ void test_write_read_config() {
     assert(config->entries[2].size == strlen("Hello, World!") + 1);
     assert(strcmp((char*)config->entries[2].value, "Hello, World!") == 0);
 
+    assert(strcmp(config->entries[3].key, "structure") == 0);
+    assert(config->entries[3].type == CONFIN_ANNOTYPE_STRUCT);
+    struct Structure structure;
+    assert(config->entries[3].size == sizeof(structure));
+    memcpy(&structure, config->entries[3].value, sizeof(structure));
+    assert(structure.x == 20 && structure.y == 40);
+
     // Free the configuration file structure
     cffreecfgfile(config);
 }
@@ -65,7 +80,7 @@ void test_write_read_config() {
 void test_validate_config() {
     // Valid
     {
-        cfentry_t entries[3];
+        cfentry_t entries[4];
         uint64_t entry_count;
         create_sample_entries(entries, &entry_count);
 
