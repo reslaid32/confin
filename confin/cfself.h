21540af4e12e9d4d4ee29ddc166f2d6075ed75c1
Hayes <zangetsuebaw@gmail.com>
2024-07-17 11:28:28 +0300

    Create cfself.h
    Added main header file

---
+++ confin/cfself.h
@@ -0,0 +1,11 @@
+#ifndef _CONFIN_SELF_H
+#define _CONFIN_SELF_H
+
+/* Main header to include, includes all confin library headers */
+
+#include "cfstruct.h" // confin/cfstruct.h
+#include "cftype.h" // confin/cftype.h
+#include "cfio.h" // confin/cfio.h
+#include "cfutils.h" // confin/cfutil.h
+
+#endif // _CONFIN_SELF_H
